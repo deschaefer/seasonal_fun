595c1ff60bd64b51da965726943abc3ef6a966f4
deschaefer <deschaefer@gmail.com>
2024-12-08 22:17:25 -0500

    five a and b

---
+++ five.py
@@ -0,0 +1,90 @@
+
+def read_rules():
+
+    filename = 'input_5_rules.txt'
+    rules_map = {}
+
+    with open(filename, 'r') as file:
+        for line in file:
+            line = line.strip()
+            left, right = list(line.split('|'))
+            if left in rules_map:
+                rules_map[left].append(right)
+            else:
+                rules_map[left] = [right]
+
+    return rules_map
+
+
+def read_pages():
+
+    filename = 'input_5_pages.txt'
+
+    pages = []
+
+    with open(filename, 'r') as file:
+        for line in file:
+            line = line.strip()
+            new_pages = list(line.split(','))
+            pages.append(new_pages)
+    
+    return pages
+
+
+def validate_page(rules_map, page):
+    for index, chapter in enumerate(page):
+        if rules_map.get(chapter) is None:
+            continue
+        rules = rules_map[chapter]
+        for rule in rules:
+            try:
+                pos = page.index(rule)
+                if pos < index:
+                    return False, 0
+            except ValueError:
+                pass
+    print(f'pass {page}')
+    return True, page[len(page) // 2]
+
+
+def fix_page(rules_map, page):
+    for index, chapter in enumerate(page):
+        if rules_map.get(chapter) is None:
+            continue
+        rules = rules_map[chapter]
+        for rule in rules:
+            try:
+                pos = page.index(rule)
+                if pos < index:
+                    page[index], page[pos] = page[pos], page[index]
+                    return False, 0, page
+            except ValueError:
+                pass
+    print(f'pass {page}')
+    return True, page[len(page) // 2], page
+
+
+def validate(rules_map, pages):
+    sum = 0
+    for page in pages:
+        valid, middle_item = validate_page(rules_map, page)
+        if not valid:
+            while not valid:
+                valid, middle_item, page = fix_page(rules_map, page)
+                valid, middle_item = valid, middle_item = validate_page(rules_map, page)
+            sum += int(middle_item)
+    return sum
+
+
+def main():
+    rules_map = read_rules()
+    print(rules_map)
+    pages = read_pages()
+    print(pages)
+
+    validated = validate(rules_map, pages)
+    print(validated)
+
+
+if __name__ == '__main__':
+    main()
---
+++ five_old.py
@@ -0,0 +1,92 @@
+
+class TreeNode:
+    def __init__(self, name, children=None):
+        self.name = name
+        self.children = children if children else []
+
+    def add_child(self, child):
+        self.children.append(child)
+
+    def remove_child(self, child):
+        for i, value in enumerate(self.children):
+            if value.name == child.name:
+                return self.children.pop(i)
+        return None
+
+    def find(self, name):
+        for i, value in enumerate(self.children):
+            if value.name == name:
+                return value
+        return None
+
+    @staticmethod
+    def find_parent(node, name):
+        if node.name == name:
+            return None
+        
+        if node.find(name):
+            return node
+        
+        for child in node.children:
+            found = TreeNode.find_parent(child, name)
+            if found:
+                return found
+        
+        return None
+
+
+    @staticmethod
+    def find_child(node, name):
+        
+        if node.name == name:
+            return node
+        
+        for child in node.children:
+            found = TreeNode.find_child(child, name)
+            if found:
+                return found
+        
+        return None
+
+    def __str__(self, level=0):
+        ret = " " * (level * 4) + str(self.name) + "\n"
+        for child in self.children:
+            ret += child.__str__(level + 1)
+        return ret
+
+
+def read_rules():
+
+    filename = 'input_5_a_rules.txt'
+
+    tree = TreeNode('root')
+
+    with open(filename, 'r') as file:
+        for line in file:
+            line = line.strip()
+            left, right = list(line.split('|'))
+            left_node = TreeNode.find_child(tree, left)
+            right_node = TreeNode.find_child(tree, right)
+
+            if not left_node and not right_node:
+                left_node = TreeNode(left)
+                tree.add_child(left_node)
+                left_node.add_child(TreeNode(right))
+            elif left_node and not right_node:
+                left_node.add_child(TreeNode(right))
+            elif left_node and right_node:
+                # need to move nodes around
+                parent_node = TreeNode.find_parent(tree, right)
+                parent_node.remove_child(right_node)
+                left_node.add_child(right_node)
+
+    return tree
+
+
+def main():
+    tree = read_rules()
+    print(tree)
+
+
+if __name__ == '__main__':
+    main()
